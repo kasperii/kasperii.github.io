5d23daeb6ff821d33815b1cf5445736d8e5a0c02
kasperii <kasperka@kth.se>
2016-07-05 15:48:23 +0900

    New work-icons
    Added 8 work info squares,  now filled with images of seals. Also added
    a sliding cover page when you hover over a picture. The icon is too
    colourful, but right now as the goal is only to complete not design -
    it will do!

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,21 @@
-$( document ).ready(function() {
-
-  // Get started!
-
+//$( document ).ready(function()
+$(function(){
+    smoothScroll(300);
 });
+  // Get started!
+    function smoothScroll (duration) {
+        $('a[href^="#"]').on('click', function(event) {
+            //So it looks for href with # in . if click next function starts
+            var target = $( $(this).attr('href') );
+            //now it stores the name from the # as target
+            if( target.length ) {
+                //if its not empty
+                event.preventDefault();
+                //prevent acting like links usually acts like (following the hyperlink) like reloading the browser
+                $('html, body').animate({
+                    // go to target.offset which is the offset / id (to top? or from top?)
+                    scrollTop: target.offset().top
+                }, duration);
+            }
+        });
+    }
